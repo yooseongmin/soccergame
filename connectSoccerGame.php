e38da54bc2a88093c488e86b491d98aac1a044b6
유성민 <andy@yuseongmin-ui-MacBookPro.local>
2019-08-24 12:28:26 +0900

    유성민 수정

---
+++ connectSoccerGame.php
@@ -0,0 +1,11 @@
+<?php
+    $servername = "localhost";
+    $username = "cocoa900";
+    $password = "a@741852963";
+    $dbname = "cocoa900";
+
+    $conn = new mysqli($servername,$username,$password,$dbname);
+    if($conn->connect_error){
+        die("연결 실패 : ".$conn->connect_error);
+    }
+?>
