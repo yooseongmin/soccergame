fbee096c1d51efcb611748405b79e82982432d8a
유성민 <andy@yuseongmin-ui-MacBookPro.local>
2019-09-01 14:10:51 +0900

    JSON 공부하기

--- searchPlayer.php
+++ searchPlayer.php
@@ -5,8 +5,16 @@
     
     $sql = "SELECT name,team FROM MyPlayer WHERE name LIKE '%$_name%'";
     $result = $conn->query($sql);
+    $result_text = "[";
+    $i = 0;
     while($row = mysqli_fetch_array($result)){
-        echo '<p>'.$row['name'].$row['team'].'</p>';
+        if($i>0){
+            $result_text= $result_text.",";
+        }
+        $result_text = $result_text.json_encode($row);
+        $i++;
     }
+    $result_text = $result_text."]";
+    echo $result_text;
   
 ?>
