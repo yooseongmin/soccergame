12ab9ddc5e1a1f9e2e44933b0e63130b06e86a69
유성민 <andy@yuseongmin-ui-MacBookPro.local>
2019-10-08 00:19:52 +0900

    깨달음

--- loadPlayer.php
+++ loadPlayer.php
@@ -1,23 +1,22 @@
 <?php
-    require_once "connectSoccerGame.php";
+    require_once "connectDataBase.php";
 
     $name = $_POST["name"];
-    $sql = "SELECT _name FROM Myplayer WHERE _name = '$name'";
 
-    $result = $conn -> query($sql);
+    
+    $sql = "SELECT * FROM SoccerGame WHERE _name = '$name'";
+
+    $result = $conn->query($sql);
     $result_text = "[";
     $i = 0 ;
 
-    while($row = mysqli_fetch_array($result)){
-        if($i>0){
-            $result_text = $result_text.",";
-        }
-        $result_text = $result_text.json_encode($row);
-        $i++;
-    }
+    $row = mysqli_fetch_row($result);
 
-   $result_text = $result_text."]";
+
+
+
    
-   echo $result_text;
+   
+   echo print_r($row);
 
 ?>
